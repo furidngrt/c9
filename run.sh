9190a497e3a1d6bd83a0871488fcd36ef75268d4
furi dngrt <63885192+furidngrt@users.noreply.github.com>
2022-12-13 09:02:13 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\e[1m\e[32m1. Updating... \e[0m" && sleep 1
+# update
+sudo apt-get update && sudo apt-get -y install curl git build-essential
+
+echo -e "\e[1m\e[32m2. Installing packages... \e[0m" && sleep 1
+# packages
+curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+
+# install nodejs
+sudo apt-get -y install nodejs
+
+echo -e "\e[1m\e[32m3. Downloading c9 and build... \e[0m" && sleep 1
+# download and build binaries
+git clone https://github.com/furidngrt/c9.git sdk
+cd sdk
+./scripts/install-sdk.sh
